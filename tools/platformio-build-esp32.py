1940dc7ac1d8e9c9d79101af80a8be9e582db710
chegewara <imperiaonline4@gmail.com>
2022-11-17 07:09:55 +0100

    Enable LWIP PPP support

--- tools/platformio-build-esp32.py
+++ tools/platformio-build-esp32.py
@@ -323,7 +323,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4.2-378-g9269a536ac\\"'),
+        ("IDF_VER", '\\"v4.4.3\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
